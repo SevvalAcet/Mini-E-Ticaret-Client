45a1e7d47b2bafc69272ab3a94f393f1c7e1d666
SevvalAcet <83153151+SevvalAcet@users.noreply.github.com>
2023-04-09 23:17:34 +0300

    Bootstrap Jquery Entegrasyonu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+declare var $: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'ETicaretClient';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
-    //BrowserAnimationsModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     AdminModule,UiModule, BrowserAnimationsModule
   ],
